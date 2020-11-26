c693f9f0cdf3faa03f63ba520397d6a8356a19b1
nandaputriafifah <nanda@paola.id>
2020-11-26 18:13:08 +0700

    display detail batik

--- project/src/app/home/list-batik/detail-batik/detail-batik.page.ts
+++ project/src/app/home/list-batik/detail-batik/detail-batik.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { AngularFireDatabase } from '@angular/fire/database';
+import {AngularFireDatabase, snapshotChanges} from '@angular/fire/database';
+import {BatikService} from "../../batik.service";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-detail-batik',
@@ -10,21 +12,44 @@ import { AngularFireDatabase } from '@angular/fire/database';
 export class DetailBatikPage implements OnInit {
 dataURL: string;
 batikDetail:any;
+batiks: any;
+batikName: string;
+batikHistory: string;
+batikImage: string;
+batikDescription: string;
+batikProvince: string;
+
   constructor(
       private activatedRoute: ActivatedRoute,
       public afDatabase: AngularFireDatabase,
+      public batikService: BatikService
   ) {
   }
 
     ngOnInit() {
-    this.activatedRoute.paramMap.subscribe(paramMap => {
-      this.dataURL = paramMap.get('batik_id');
-    });
-    this.batikDetail = this.getBatik(this.dataURL);
+      this.batikService.getAllBatik().snapshotChanges().pipe(
+          map(changes =>
+              changes.map(c => ({batik_id: c.payload.key, ...c.payload.val()}))
+          )
+      ).subscribe(data => {
+        this.batiks = data;
+        console.log('DATA', data);
+        this.activatedRoute.paramMap.subscribe(paramMap => {
+          this.dataURL = paramMap.get('batik_id');
+        });
+        this.batikDetail = this.getBatik(this.dataURL);
+      });
   }
-   getBatik(id) {
-    this.afDatabase.object('batik/' + id ).valueChanges().subscribe(val => {
-      this.batikDetail = val;
+
+  getBatik(id) {
+    let ref =  this.afDatabase.database.ref('batik/' + id);
+    ref.once('value').then((snapshot) => {
+        this.batikDetail = snapshot.val();
+        this.batikName = this.batikDetail.batik_name;
+        this.batikDescription = this.batikDetail.batik_description;
+        this.batikProvince = this.batikDetail.province_name;
+        this.batikHistory = this.batikDetail.batik_history;
+        this.batikImage = this.batikDetail.batik_image;
     });
   }
 
